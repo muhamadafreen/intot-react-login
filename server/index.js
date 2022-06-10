a44cb81172d1f1a838563a3bba06797b84e1743b
Muhamad Afreen <muhamadafreen111@gmail.com>
2022-06-10 18:21:01 +0530

    First commit

--- server/index.js
+++ server/index.js
@@ -1,32 +1,71 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require("cors");
+const express = require("express");
+const mongoose = require('mongoose');
+const cors =  require('cors');
 
-const app = express();
+
+const app =   express();
 
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"mysql@123",
-    database:"loginsystem"
-})
 
-app.post('/register',(req,res)=>{
+mongoose.connect('mongodb://127.0.0.1:27017/finalDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.4.2',{useNewUrlParser:true});
 
-    const username = req.body.username
-    const email = req.body.email
-    const password = req.body.password
+const userSchema =  {
+  username:String,
+  email:String,
+  password:String
+};
+const User  = new mongoose.model("User",userSchema);
 
 
-
-    db.query("INSERT INTO users(username,email,password)VALUES (?,?,?)",[username,email,password],(err,result)=>{
+app.post("/signup",function(req,res){
+    const newUser = new User({
+      username : req.body.username,
+      email:req.body.email,
+      password : req.body.password
+    });
+  
+    newUser.save(function(err,result){
+      if (err){
         console.log(err);
-    })
-})
+        res.send({err:err});
+      }
+      if(result){
+        res.send(result);
+      }
+      else{
+        res.send({message:"Successfully Registered1"});
+    
+      }
+    });
+  });
 
-app.listen(3001,  () =>{
+  app.post("/login",function(req,res){
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    User.find({username:username,email:email,password:password}, function(err, result){
+      if(err){
+        res.send({err: err});
+      }
+      
+      if (result.length >0){
+          res.send(result);
+          }else {
+              res.send({message:"Wrong username/password"});
+          }    
+      
+    });
+  })
+
+  app.get("/loggedusers",(req,res)=>{
+    User.find({})
+         .then(foundUser => res.json(foundUser))
+         
+  });
+
+app.listen(3001,() => {
     console.log("Running on port 3001");
-} )
+});
